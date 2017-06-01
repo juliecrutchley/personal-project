fe3dbcd3ac3c373036c44b2ba36c9567b2e89747
EDA <eda@mac-mini-5.local>
2017-06-01 15:49:40 +1200

    present

--- client/components/Food.jsx
+++ client/components/Food.jsx
@@ -10,80 +10,61 @@ const Food = (props) => {
 
   return (
     <div className='food col-md-12 col-xs-12'>
-        <div className='food-name col-md-12 col-xs-12'>
-          <h4>{props.food.name}</h4>
+      <div className='food-name col-md-12 col-xs-12'>
+        <h4>{props.food.name}</h4>
+      </div>
+
+      <div className=' col-md-3 col-xs-12 healthLabels-container'>
+        <div className='title'>
+          <h5>Health Labels</h5>
         </div>
-
-        <div className=' col-md-4 col-xs-12 healthLabels-container'>
-          <div>
-            <h5>Health Labels</h5>
-          </div>
-            {healthLabels.map((label) => {
-              return (
-                <div className='healthLabel'>
-                  {label.split('_').join(' ')}
-                </div>
-              )
-            })}
-          </div>
-
-
-
-
-          <div className='macro-header col-md-4 col-xs-12'>
-            <h5>Macro information</h5>
-            <div className='macro-name-container'>
-              <div className='macro name sugar'>
-                <p>Sugar</p>
-              </div>
-              <div className='macro name fat'>
-                <p>Fat</p>
-              </div>
-              <div className='macro name calories'>
-                <p>Calories</p>
-              </div>
-              <div className='macro-amount-container'>
-                <div className='macro amount sugar'>
-                  <p>{`${props.food.sugarActual} g`}</p>
-                </div>
-                <div className='macro amount fat'>
-                  <p>{`${props.food.fatActual} g`}</p>
-                </div>
-                <div className='macro amount calories'>
-                  <p>{props.food.calories}</p>
-                </div>
-              </div>
+        {healthLabels.map((label) => {
+          return (
+            <div className='healthLabel'>
+              {label.split('_').join(' ')}
             </div>
+          )
+        })}
+      </div>
+
+      <div className='macro-header title col-md-4 col-xs-12'>
+        <h5>Macro information</h5>
+        <div className='macro-name-container'>
+          <div className='macro name sugar'>
+            <div className='head'>Sugar</div>
+            <div>{`${props.food.sugarActual} g`}</div>
           </div>
-
-
-          <div className='exercise-header col-md-4 col-xs-12'>
-            <h5>Exercise options to burn this off</h5>
-            <div className='exercise-name-container'>
-              <div className='exercise running'>
-                <p>Running</p>
-              </div>
-              <div className='exercise mowing'>
-                <p>Mowing Lawns</p>
-              </div>
-              <div className='exercise hacky'>
-                <p>Playing Hacky Sack</p>
-              </div>
-              <div className='exercise-amount-container'>
-                <div className='exercise amount running'>
-                  <p>{`${running} minutes`}</p>
-                </div>
-                <div className='exercise amount mowing'>
-                  <p>{`${mowing} minutes`}</p>
-                </div>
-                <div className='exercise amount hacky'>
-                  <p>{`${hacky} minutes`}</p>
-                </div>
-            </div>
-            </div>
+          <div className='macro name fat'>
+            <div className='head'>Fat</div>
+            <div>{`${props.food.fatActual} g`}</div>
           </div>
+          <div className='macro name calories'>
+            <div className='head'>Calories</div>
+            <div>{props.food.calories}</div>
+          </div>
+        </div>
+      </div>
 
-
+      <div className='exercise-header title col-md-4 col-xs-12'>
+        <h5>Exercise options to burn this off</h5>
+        <div className='exercise-name-container'>
+          <div className='exercise running'>
+            <div className='head'>Running</div>
+            <div>{`${running} minutes`}</div>
+          </div>
+          <div className='exercise mowing'>
+            <div className='head'>Mowing Lawns</div>
+            <div>{`${mowing} minutes`}</div>
+          </div>
+          <div className='exercise hacky'>
+            <div className='head'>Playing Hacky Sack</div>
+            <div>{`${hacky} minutes`}</div>
+          </div>
+        </div>
+      </div>
+      <div className='extra col-md-8 col-xs-12 more'>
+        <p>More stuff is supposed to go here</p>
+      </div>
     </div>
   )
 }
